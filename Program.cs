e72f79d19a05b6a96e67fe2abbe4421539eb5150
Matt <mateusz.pianka@outlook.com>
2015-03-12 01:49:34 +0100

    added launch argument / 1.0.1

--- Program.cs
+++ Program.cs
@@ -13,7 +13,12 @@ namespace BingWallpaperBrowser
 
             // if any args were given
             if ( args.Length > 0 ) {
-                // ...do something
+                if ( args[ 0 ] == "/a" | args[ 0 ] == "/auto" ) {
+                    using ( Wallpaper w = new Wallpaper() ) {
+                        Bing b = new Bing();
+                        w.Set( Wallpaper.WallpaperStyle.Fit, b.GetWallpaper( 0 ).Url );
+                    }
+                }
             } else {
                 // show main window
                 p.MainWindow = new View.Browser();
